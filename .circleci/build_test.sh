fbf109efbe5b0df312321cd666e34f9591efb478
Marin Baron <marin6314@gmail.com>
2019-05-08 18:10:37 +0200

    Update CircleCI pipeline (31)

--- .circleci/build_test.sh
+++ .circleci/build_test.sh
@@ -5,7 +5,6 @@ git clone https://github.com/Niram7777/nvidia-docker.git
 make ubuntu-latest -C nvidia-docker &> nvidia-docker.log
 docker build --build-arg U_ID="$(id -u)" --build-arg G_ID="$(id -g)" -t rpcs3 - < Dockerfile &> rpcs3-docker.log
 source .dockerrc
-ls *
 docker_rpcs3 -u -m -t -c
 bash <(curl -s https://codecov.io/bash) -s coverage/
 
